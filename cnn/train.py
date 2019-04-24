92cac4319f1c7b313075769771d19d34051219bf
joneswong <joneswong.ml@gmail.com>
2019-04-24 17:04:52 +0800

    support various versions

--- cnn/train.py
+++ cnn/train.py
@@ -130,9 +130,14 @@ def train(train_queue, model, criterion, optimizer):
 
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
     n = input.size(0)
-    objs.update(loss.data.item(), n)
-    top1.update(prec1.data.item(), n)
-    top5.update(prec5.data.item(), n)
+    # latest
+    #objs.update(loss.data.item(), n)
+    #top1.update(prec1.data.item(), n)
+    #top5.update(prec5.data.item(), n)
+    # v0.3.1
+    objs.update(loss.data[0], n)
+    top1.update(prec1.data[0], n)
+    top5.update(prec5.data[0], n)
 
     if step % args.report_freq == 0:
       logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
@@ -155,9 +160,14 @@ def infer(valid_queue, model, criterion):
 
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
     n = input.size(0)
-    objs.update(loss.data.item(), n)
-    top1.update(prec1.data.item(), n)
-    top5.update(prec5.data.item(), n)
+    # latest
+    #objs.update(loss.data.item(), n)
+    #top1.update(prec1.data.item(), n)
+    #top5.update(prec5.data.item(), n)
+    # v0.3.1
+    objs.update(loss.data[0], n)
+    top1.update(prec1.data[0], n)
+    top5.update(prec5.data[0], n)
 
     if step % args.report_freq == 0:
       logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
